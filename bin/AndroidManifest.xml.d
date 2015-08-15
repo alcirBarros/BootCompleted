b17ff1300e2bad66b6375488adb92531e5bf4419
alci <alci@debian.home>
2015-08-15 12:29:14 -0300

    #4155 - [Dispensação Produto] Ativa "Foreign key" 'fk_pdp_ses_id'

--- bin/AndroidManifest.xml.d
+++ bin/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/home/alci/NetBeansProjects/BootCompleted/bin/AndroidManifest.xml : \
-/home/alci/NetBeansProjects/BootCompleted/AndroidManifest.xml \
+/home/alci/Java/Projetos/Git/BootCompleted/bin/AndroidManifest.xml : \
+/home/alci/Java/Projetos/Git/BootCompleted/AndroidManifest.xml \
--- bin/classes.dex.d
+++ bin/classes.dex.d
@@ -1,11 +1,11 @@
-/home/alci/NetBeansProjects/BootCompleted/bin/classes.dex : \
-/home/alci/NetBeansProjects/BootCompleted/bin/classes/br/com/Mensagem.class \
-/home/alci/NetBeansProjects/BootCompleted/bin/classes/br/com/R$layout.class \
-/home/alci/NetBeansProjects/BootCompleted/bin/classes/br/com/BootReceiver.class \
-/home/alci/NetBeansProjects/BootCompleted/bin/classes/br/com/R.class \
-/home/alci/NetBeansProjects/BootCompleted/bin/classes/br/com/R$attr.class \
-/home/alci/NetBeansProjects/BootCompleted/bin/classes/br/com/BuildConfig.class \
-/home/alci/NetBeansProjects/BootCompleted/bin/classes/br/com/R$string.class \
-/home/alci/NetBeansProjects/BootCompleted/bin/classes/br/com/R$drawable.class \
-/home/alci/NetBeansProjects/BootCompleted/bin/classes/br/com/MainActivity.class \
-/home/alci/NetBeansProjects/BootCompleted/bin/classes/br/com/R$id.class \
+/home/alci/Java/Projetos/Git/BootCompleted/bin/classes.dex : \
+/home/alci/Java/Projetos/Git/BootCompleted/bin/classes/br/com/Mensagem.class \
+/home/alci/Java/Projetos/Git/BootCompleted/bin/classes/br/com/R$layout.class \
+/home/alci/Java/Projetos/Git/BootCompleted/bin/classes/br/com/BootReceiver.class \
+/home/alci/Java/Projetos/Git/BootCompleted/bin/classes/br/com/R.class \
+/home/alci/Java/Projetos/Git/BootCompleted/bin/classes/br/com/R$attr.class \
+/home/alci/Java/Projetos/Git/BootCompleted/bin/classes/br/com/BuildConfig.class \
+/home/alci/Java/Projetos/Git/BootCompleted/bin/classes/br/com/R$string.class \
+/home/alci/Java/Projetos/Git/BootCompleted/bin/classes/br/com/R$drawable.class \
+/home/alci/Java/Projetos/Git/BootCompleted/bin/classes/br/com/MainActivity.class \
+/home/alci/Java/Projetos/Git/BootCompleted/bin/classes/br/com/R$id.class \
