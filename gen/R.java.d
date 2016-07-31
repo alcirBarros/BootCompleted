4f1bd42aa361757156e80e2f969eb074869e705d
alci <alci@debian.home>
2016-07-31 14:52:20 -0300

    #5897 - [Relatório] Desenvolver um relatório de inventário por data e estoque

--- gen/R.java.d
+++ gen/R.java.d
@@ -5,4 +5,8 @@
 /home/alci/Java/Projetos/Git/BootCompleted/res/drawable-mdpi/ic_launcher.png \
 /home/alci/Java/Projetos/Git/BootCompleted/res/drawable-ldpi/ic_launcher.png \
 /home/alci/Java/Projetos/Git/BootCompleted/res/layout/main.xml \
+/home/alci/Java/Projetos/Git/BootCompleted/bin/res/drawable-hdpi/ic_launcher.png \
+/home/alci/Java/Projetos/Git/BootCompleted/bin/res/drawable-xhdpi/ic_launcher.png \
+/home/alci/Java/Projetos/Git/BootCompleted/bin/res/drawable-mdpi/ic_launcher.png \
+/home/alci/Java/Projetos/Git/BootCompleted/bin/res/drawable-ldpi/ic_launcher.png \
 /home/alci/Java/Projetos/Git/BootCompleted/bin/AndroidManifest.xml \
